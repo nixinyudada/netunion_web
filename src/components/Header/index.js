b441878b566926df33ad75ecd957fbc3e0bbe36f
nixinyudada <940306566@qq.com>
2018-11-27 23:41:04 +0800

    inir

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./index.css"
+
+export default class Header extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>header...</h1>
+            </div>
+        );
+    }
+}
