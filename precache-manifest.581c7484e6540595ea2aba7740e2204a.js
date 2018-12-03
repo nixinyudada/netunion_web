293b0d8d4ca8dd333b8fa296137bec65efce63e8
nixinyudada <940306566@qq.com>
2018-12-03 10:15:49 +0800

    Updates

---
+++ precache-manifest.581c7484e6540595ea2aba7740e2204a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "22162056daab57b06b7b",
+    "url": "/static/js/main.22162056.chunk.js"
+  },
+  {
+    "revision": "4b01872be60b887f2310",
+    "url": "/static/js/1.4b01872b.chunk.js"
+  },
+  {
+    "revision": "22162056daab57b06b7b",
+    "url": "/static/css/main.71f82967.chunk.css"
+  },
+  {
+    "revision": "4b01872be60b887f2310",
+    "url": "/static/css/1.6093149c.chunk.css"
+  },
+  {
+    "revision": "2a9a68739b2e76f40d54c8b4fdaed3c3",
+    "url": "/index.html"
+  }
+];
