9500eaa9973c2cddd07e337da73721beeeb1f102
nixinyudada <940306566@qq.com>
2018-12-06 17:49:31 +0800

    Updates

---
+++ precache-manifest.e6ca6656f716a28682a125b79a4d706e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e011c04977d680103f3",
+    "url": "/netunion_web/static/js/runtime~main.9e011c04.js"
+  },
+  {
+    "revision": "19017c557d15a58109b5",
+    "url": "/netunion_web/static/js/main.19017c55.chunk.js"
+  },
+  {
+    "revision": "645bc1dde6f59cd0bbc1",
+    "url": "/netunion_web/static/js/1.645bc1dd.chunk.js"
+  },
+  {
+    "revision": "19017c557d15a58109b5",
+    "url": "/netunion_web/static/css/main.81aa8a9d.chunk.css"
+  },
+  {
+    "revision": "645bc1dde6f59cd0bbc1",
+    "url": "/netunion_web/static/css/1.4c922835.chunk.css"
+  },
+  {
+    "revision": "4abfbd1d0faa60580ce04c7ffb579559",
+    "url": "/netunion_web/index.html"
+  }
+];
