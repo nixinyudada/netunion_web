1a2967d3da7ec5b217f4ec0038e77b7c903def01
nixinyudada <940306566@qq.com>
2018-12-21 03:48:05 +0800

    更新故障状态

--- service/Router/router.js
+++ service/Router/router.js
@@ -8,79 +8,85 @@ var router = express.Router()
 var LogincaptchaCode = ""
 var dormFixCaptchaCode = ""
 var isLogin = false
-router.get('/changeCaptcha',(req,res)=>{
-    try{
+router.get('/changeCaptcha', (req, res) => {
+    try {
         let codeConfig = {
             size: 5,// 验证码长度
             ignoreChars: '0o1i', // 验证码字符中排除 0o1i
             noise: 2, // 干扰线条的数量
-            height: 44 
+            height: 44
         }
         let captcha = svgCaptcha.create(codeConfig);
         req.session.captcha = captcha.text.toLowerCase()
         // console.log(req.session.captcha)
         LogincaptchaCode = captcha.text.toLowerCase()
         res.jsonp({
-            "img":captcha.data
+            "img": captcha.data
         })
-    }catch(e){
+    } catch (e) {
         res.jsonp({
-            "img":"异常"
+            "img": "异常"
         })
     }
 })
 
 // 验证码是否正确？
-router.post('/captcha',(req,res)=>{
-    try{
-        res.setHeader("Access-Control-Allow-Origin","*")
-    let clientCaptch = req.body
-    if (LogincaptchaCode === clientCaptch.captcha){
+router.post('/captcha', (req, res) => {
+    try {
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        let clientCaptch = req.body
+        if (LogincaptchaCode === clientCaptch.captcha) {
+            res.jsonp({
+                "captcha": true
+            })
+        } else {
+            res.jsonp({
+                "captcha": false
+            })
+        }
+    } catch (e) {
         res.jsonp({
-            "captcha":true
-        })
-    }else{
-        res.jsonp({
-            "captcha":false
-        })
-    }
-    }catch(e){
-        res.jsonp({
-            "captcha":false
+            "captcha": false
         })
     }
 })
 
 
 // 登录接口
-router.post('/loginify',(req,res)=>{
-    try{
-        res.setHeader("Access-Control-Allow-Origin","*")
-    User.login(req.body,(bool,str)=>{
-        if(bool){
-            isLogin = true
-        }
-        res.send({
-            msg:str,
-            bool:bool
+router.post('/loginify', (req, res) => {
+    try {
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        User.login(req.body, (bool, str) => {
+            if (bool) {
+                isLogin = true
+            }
+            res.send({
+                msg: str,
+                bool: bool
+            })
         })
-    })
-    }catch(e){
+    } catch (e) {
         res.send({
-            msg:"异常",
-            bool:false
+            msg: "异常",
+            bool: false
         })
     }
 })
 
 // 是否登录的 session
 
-router.get('/loginSession',(req,res)=>{
-    res.setHeader("Access-Control-Allow-Origin","*")
-    req.session.isLogin = isLogin
+router.get('/loginSession', (req, res) => {
+    try{
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        req.session.isLogin = isLogin
         res.send({
-            loginSession:req.session.isLogin,
+            loginSession: req.session.isLogin,
         })
+    }catch(e){
+        res.send({
+            loginSession: "异常",
+        })
+    }
 })
 
 
@@ -90,100 +96,208 @@ router.get('/loginSession',(req,res)=>{
 // 报修
 
 // 提交故障报修的验证码
-router.get('/dormFixCaptchaImg',(req,res)=>{
-    try{
-        res.setHeader("Access-Control-Allow-Origin","*")
-    let codeConfig2 = {
-        size: 5,// 验证码长度
-        ignoreChars: '0o1i', // 验证码字符中排除 0o1i
-        noise: 2, // 干扰线条的数量
-        height: 44 
-    }
-    let captcha2 = svgCaptcha.create(codeConfig2);
-    dormFixCaptchaCode = captcha2.text.toLowerCase()
-    if(captcha2){
+router.get('/dormFixCaptchaImg', (req, res) => {
+    try {
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        let codeConfig2 = {
+            size: 5,// 验证码长度
+            ignoreChars: '0o1i', // 验证码字符中排除 0o1i
+            noise: 2, // 干扰线条的数量
+            height: 44
+        }
+        let captcha2 = svgCaptcha.create(codeConfig2);
+        dormFixCaptchaCode = captcha2.text.toLowerCase()
+        if (captcha2) {
+            res.jsonp({
+                "img": captcha2.data
+            })
+        } else {
+
+        }
+    } catch (e) {
         res.jsonp({
-            "img":captcha2.data
-        })
-    }else{
-        
-    }
-    }catch(e){
-        res.jsonp({
-            "img":"异常！"
+            "img": "异常！"
         })
     }
-    
+
 })
 
-router.post('/dormFixCaptchaText',(req,res)=>{
-    try{
-        res.setHeader("Access-Control-Allow-Origin","*")
-    if (req.body.captchaText === dormFixCaptchaCode){
-        var data = {
-            dormNum:req.body.dormNum,
-            callNum:req.body.callNum,
-            userDescr:req.body.userDescr,
-            findDate:req.body.findDate,
-            state:req.body.state,
-            adminLog:req.body.adminLog,
-            admingUpdateTime:req.body.admingUpdateTime
+router.post('/dormFixCaptchaText', (req, res) => {
+    try {
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        if (req.body.captchaText === dormFixCaptchaCode) {
+            var data = {
+                dormNum: req.body.dormNum,
+                callNum: req.body.callNum,
+                userDescr: req.body.userDescr,
+                findDate: req.body.findDate,
+                state: req.body.state,
+                adminLog: req.body.adminLog,
+                admingUpdateTime: req.body.admingUpdateTime
+            }
+
+            DormFix.DormFixInsert(data, (bool, msg) => {
+                res.jsonp({
+                    "codeify": true,
+                    "msg": msg,
+                    "msgBool": bool
+                })
+            })
+
+        } else {
+            res.jsonp({ "codeify": false })
+        }
+    } catch (e) {
+        res.send({
+            "msg": "异常！",
+            "msgBool": false
+        })
+    }
+
+})
+
+
+
+router.post('/dormFixInsert', (req, res) => {
+    try {
+        res.setHeader("Access-Control-Allow-Origin", "*")
+
+        var data1 = {
+            dormNum: "3-556",
+            callNum: "16602803351",
+            userDescr: "This is descriptions！",
+            findDate: "2018-05-16 22:10:10",
+            state: 0,
+            adminLog: "This is admin log!",
+            admingUpdateTime: new Date()
         }
 
-        DormFix.DormFixInsert(data,(bool,msg)=>{
+
+        DormFix.DormFixInsert(data1, (bool, msg) => {
             res.jsonp({
-                "codeify":true,
-                "msg":msg,
-                "msgBool":bool
+                "msg": msg,
+                "msgBool": bool
             })
         })
-
-    }else{
-        res.jsonp({ "codeify":false })
-    }
-    }catch(e){
-        res.send({
-            "msg":"异常！",
-            "msgBool":false
+    } catch (e) {
+        res.jsonp({
+            "msg": "异常",
+            "msgBool": false
         })
     }
-    
+
+
 })
 
 
 
-router.post('/dormFixInsert',(req,res)=>{
+
+// 获取各个状态的数据
+router.get('/DormFixListState', (req, res) => {
+    try {
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        DormFix.DormFixStateCount((msgBool, msgArr) => {
+            if (msgBool) {
+                console.log(msgArr)
+                res.send({
+                    msgBool:msgBool,
+                    msgArr:msgArr
+                })
+            } else {
+                res.send({
+                    msgBool:false,
+                    msgArr:["!","!","!","!"]
+                })
+            }
+        })
+
+    } catch (e) {
+        res.send("异常！")
+    }
+})
+
+
+// 获取数据库中不同 state 的数据
+
+router.get('/DormFixList', (req, res) => {
+    try {
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        DormFix.DormFixList(req.query.state, (msgBool, msg) => {
+            if (msgBool) {
+                // 模拟网络延迟
+                setTimeout(()=>{
+                    res.send({
+                        msg:true,
+                        data:msg
+                    })
+                },500)
+            } else {
+                res.send({
+                    msg:"查询失败！",
+                    data:null
+                })
+            }
+        })
+
+    } catch (e) {
+        res.send("异常！")
+    }
+})
+
+// 根据 id 查询详细数据
+router.get('/DormFixWhere', (req, res) => {
+    try {
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        let id = req.query.id
+        console.log(id)
+        
+        DormFix.DormFixDetail(id,(msgBool,ret)=>{
+            if (msgBool) {
+                setTimeout(()=>{
+                    res.send({
+                        msg:true,
+                        data:ret
+                    })
+                },2000)
+            } else {
+                res.send({
+                    msg:"查询失败！",
+                    data:null
+                })
+            }
+        })
+    } catch (e) {
+        res.send("异常！")
+    }
+})
+
+
+// 更改故障报修的维护状态
+
+router.post('/DormFixUpdateState',(req,res)=>{
     try{
-        res.setHeader("Access-Control-Allow-Origin","*")
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        console.log(req.body.id)
+        console.log(req.body.updateStateVal)
 
-    var data1 = {
-        dormNum:"3-556",
-        callNum:"16602803351",
-        userDescr:"This is descriptions！",
-        findDate:"2018-05-16 22:10:10",
-        state:0,
-        adminLog:"This is admin log!",
-        admingUpdateTime:new Date()
-    }
-
-
-    DormFix.DormFixInsert(data1,(bool,msg)=>{
-        res.jsonp({
-            "msg":msg,
-            "msgBool":bool
+        DormFix.DormFixUpdateState(req.body.id,req.body.updateStateVal,(msgBool,msg)=>{
+            if(msgBool){
+                res.send({
+                    msg:true
+                })
+            }else{
+                res.send({
+                    msg:false
+                })
+            }
         })
-    })
     }catch(e){
-        res.jsonp({
-            "msg":"异常",
-            "msgBool":false
+        console.log("异常")
+        res.send({
+            msg:"异常"
         })
     }
-    
-   
 })
-
-
 
 // 导出 router
 
--- src/components/AdminComponents/DormFixList/index.js
+++ src/components/AdminComponents/DormFixList/index.js
@@ -1,57 +1,355 @@
 import React from "react";
+import $ from "jquery"
 import "./index.css"
 import {
-    Breadcrumb, Icon, Tag, Badge, Row, Col, Button, Modal
+    Breadcrumb, Icon, Tag, Badge, Row, Col, Button, Modal, Input,message,Spin
 } from 'antd';
-
+var timer = null;
 export default class DormFixList extends React.Component {
     state = {
-        checked: true,
-        loadIcon: "none",
-        getDataBtnState: false,
-        visible:false
-    };
+        DormFixID:"",
+        visible: false,
+        isDormFixOneDetail:false,
 
-    handleGetData = () => {
-        this.setState({
-            loadIcon: "inline-block",
-            getDataBtnState: true
+        isDormFixStateCount: false,
+        dormFixStateCount: "",
+
+        redLoadIcon: "none",
+        redDisabled: false,
+        redData: null,
+        redIs: false,
+
+        blueLoadIcon: "none",
+        blueDisabled: false,
+        blueData: null,
+        blueIs: false,
+
+        yellowLoadIcon: "none",
+        yellowDisabled: false,
+        yellowData: null,
+        yellowIs: false,
+
+        greenLoadIcon: "none",
+        greenDisabled: false,
+        greenData: null,
+        greenIs: false,
+
+        isDormFixDetail: false,
+        dormFixDetail: null,
+    };
+   
+    
+    initDormFixStateCount = () => {
+        let that = this
+        $.ajax({
+            url: "http://localhost:3000/DormFixListState",
+            type: "get",
+            success: function (ret) {
+                that.setState({
+                    dormFixStateCount: ret.msgArr,
+                    isDormFixStateCount: true
+                })
+                // console.log(that.state.dormFixStateCount[2])
+                // console.log(that.state.dormFixStateCount[1])
+                // console.log(that.state.dormFixStateCount[3])
+            },
+            error: function (err) {
+
+            }
         })
     }
-    handleChangeTag = () => {
-        this.setState({ checked: false });
+    componentWillMount() {
+        let that = this
+        timer = setTimeout(()=>{
+            that.initDormFixStateCount()
+            },10)
     }
-    handleTag = () => {
-        this.setState({
-            visible:true
+
+    componentWillUnmount(){
+        clearInterval(timer)
+    }
+
+    handleGetData(stateDBID) {
+
+        switch (stateDBID) {
+            case 0:
+                this.setState({
+                    redLoadIcon: "inline-block",
+                    redDisabled: true
+                })
+                break;
+            case 1:
+                this.setState({
+                    blueLoadIcon: "inline-block",
+                    blueDisabled: true
+                })
+                break;
+            case 2:
+                this.setState({
+                    yellowLoadIcon: "inline-block",
+                    yellowDisabled: true
+                })
+                break;
+            case 3:
+                this.setState({
+                    greenLoadIcon: "inline-block",
+                    greenDisabled: true
+                })
+                break;
+                default:
+                    this.setState({
+                        redLoadIcon: "none",
+        redDisabled: false,
+        redData: null,
+        redIs: false,
+
+        blueLoadIcon: "none",
+        blueDisabled: false,
+        blueData: null,
+        blueIs: false,
+
+        yellowLoadIcon: "none",
+        yellowDisabled: false,
+        yellowData: null,
+        yellowIs: false,
+
+        greenLoadIcon: "none",
+        greenDisabled: false,
+        greenData: null,
+        greenIs: false,
+
+        isDormFixDetail: false,
+        dormFixDetail: null
+                    })
+                break;
+
+        }
+
+
+        var that = this
+        $.ajax({
+            url: "http://localhost:3000/DormFixList?state=" + stateDBID,
+            type: "get",
+            success: function (ret) {
+                if (ret.msg) {
+                    switch (stateDBID) {
+                        case 0:
+                            that.setState({
+                                redData: ret.data,
+                                redIs: true,
+                                redLoadIcon: "none",
+                                redDisabled: false,
+
+                                blueIs: false,
+                                blueData: null,
+                                blueDisabled: false,
+
+                                yellowIs: false,
+                                yellowData: null,
+                                yellowDisabled: false,
+
+                                greenData: null,
+                                greenIs: false,
+                                greenDisabled: false
+                            })
+                            break;
+                        case 1:
+                            that.setState({
+                                blueData: ret.data,
+                                blueIs: true,
+                                blueLoadIcon: "none",
+                                blueDisabled: false,
+
+                                redIs: false,
+                                redData: null,
+                                redDisabled: false,
+
+                                yellowIs: false,
+                                yellowData: null,
+                                yellowDisabled: false,
+
+                                greenData: null,
+                                greenIs: false,
+                                greenDisabled: false
+                            })
+                            break;
+                        case 2:
+                            that.setState({
+                                yellowData: ret.data,
+                                yellowIs: true,
+                                yellowLoadIcon: "none",
+                                yellowDisabled: false,
+
+                                blueIs: false,
+                                blueData: null,
+                                blueDisabled: false,
+
+                                redIs: false,
+                                redData: null,
+                                redDisabled: false,
+
+                                greenData: null,
+                                greenIs: false,
+                                greenDisabled: false
+                            })
+                            break;
+                        case 3:
+                            that.setState({
+                                greenData: ret.data,
+                                greenIs: true,
+                                greenLoadIcon: "none",
+                                greenDisabled: false,
+
+                                blueIs: false,
+                                blueData: null,
+                                blueDisabled: false,
+
+                                yellowIs: false,
+                                yellowData: null,
+                                yellowDisabled: false,
+
+                                redIs: false,
+                                redData: null,
+                                redDisabled: false,
+                            })
+                            break;
+                            default:
+                            that.setState({
+                                redLoadIcon: "none",
+                redDisabled: false,
+                redData: null,
+                redIs: false,
+        
+                blueLoadIcon: "none",
+                blueDisabled: false,
+                blueData: null,
+                blueIs: false,
+        
+                yellowLoadIcon: "none",
+                yellowDisabled: false,
+                yellowData: null,
+                yellowIs: false,
+        
+                greenLoadIcon: "none",
+                greenDisabled: false,
+                greenData: null,
+                greenIs: false,
+        
+                isDormFixDetail: false,
+                dormFixDetail: null
+                            })
+                            break;
+                    }
+
+                } else {
+                    Modal.error({
+                        title: "数据获取失败了！"
+                    })
+                }
+            },
+            error: function (err) {
+
+            }
         })
+
+
+    }
+
+    handleChangeDormFixState = (event) => {
+        console.log(event.target.value)
+        let that = this
+        $.ajax({
+            url: "http://localhost:3000/DormFixUpdateState",
+            type: "post",
+            data:{
+                id:that.state.DormFixID,
+                updateStateVal:event.target.value
+            },
+            success: function (ret) {
+                if(ret.msg){
+                    message.info("更新一个维护状态")
+                    that.initDormFixStateCount()
+                }
+            },
+            error: function (err) {
+
+            }
+        })
+    }
+
+    handleTag(id) {
+       
+        this.setState({
+            visible: true,
+            DormFixID:id,
+            isDormFixOneDetail:false
+        })
+        let that = this
+        $.ajax({
+            url: "http://localhost:3000/DormFixWhere?id=" + id,
+            type: "get",
+            success: function (ret) {
+                console.log(ret.data[0])
+                that.setState({
+                    isDormFixDetail: true,
+                    dormFixDetail: ret.data[0],
+                    isDormFixOneDetail:true
+                })
+                console.log(that.state.dormFixDetail.state)
+            },
+            error: function (err) {
+
+            }
+        })
+
+
+
     }
     hideModal = () => {
         this.setState({
-            visible:false
+            visible: false
         })
     }
+
+
+
+
     render() {
         return (
             <div>
                 <Modal
-                    title={<span>报修信息&nbsp;&nbsp;<Icon style={{ display: this.state.loadIcon }} type='loading' /></span>}
+                    title="报修信息"
                     visible={this.state.visible}
                     onCancel={this.hideModal}
                     okText="确认"
                     cancelText="取消"
                 >
-                    <p></p>
-                    <p>宿舍号：</p>
-                    <p>联系方式：</p>
-                    <p>故障描述：</p>
-                    <p>故障日期：</p>
-                    <p>提交日期：</p>
-                    <p>状态标记：</p>
-                    <p>日志记录：</p>
+                <Spin spinning={!this.state.isDormFixOneDetail}>
+                    <p>宿舍号：{this.state.isDormFixDetail ? this.state.dormFixDetail.dormNum : null}</p>
+                    <p>联系方式：{this.state.isDormFixDetail ? this.state.dormFixDetail.callNum : null}</p>
+                    <p>故障描述：{this.state.isDormFixDetail ? this.state.dormFixDetail.userDescr : null}</p>
+                    <p>故障日期：{this.state.isDormFixDetail ? this.state.dormFixDetail.findDate : null}</p>
+                    <p>故障状态：</p>
+                        {/* <Select value="" id="seTest" onChange={this.handleChangeDormFixState}>
+                            <Select.Option value="0">已读</Select.Option>
+                            <Select.Option value="1">未读</Select.Option>
+                            <Select.Option value="2">已解决</Select.Option>
+                            <Select.Option value="3">未解决</Select.Option>
+                        </Select> */}
+                        <select value={this.state.isDormFixDetail ? this.state.dormFixDetail.state : ""} onChange={this.handleChangeDormFixState} id="seTest">
+                            <option value="0">未读</option>
+                            <option value="1">已读</option>
+                            <option value="2">故障未解决</option>
+                            <option value="3">故障已解决</option>
+                        </select>
+                    <p>管理日志：</p>
+                    <p><Input defaultValue={this.state.isDormFixDetail ? this.state.dormFixDetail.adminLog : ""}/></p>
+                    <p><Button>添加日志</Button></p>
+                    <p>最近修改：{this.state.isDormFixDetail ? this.state.dormFixDetail.admingUpdateTime : null}</p>
                     <p>
 
                     </p>
+                    </Spin>
                 </Modal>
                 <Breadcrumb>
                     <Breadcrumb.Item>
@@ -64,25 +362,48 @@ export default class DormFixList extends React.Component {
 
                 <Row>
                     <Col lg={{ span: 16 }}>
-                        <Tag color="red" onClick={this.handleTag}>寝室故障报修情况</Tag>
+                        {
+                            this.state.redIs ? this.state.redData.map((item, key) => {
+                                return <Tag color="red" style={{ margin: "4px 8px" }} key={key} onClick={this.handleTag.bind(this, item._id)}>{item.dormNum}</Tag>
+                            }) : false
+                        }
+
+                        {
+                            this.state.blueIs ? this.state.blueData.map((item, key) => {
+                                return <Tag color="blue" style={{ margin: "4px 8px" }} key={key} onClick={this.handleTag.bind(this, item._id)}>{item.dormNum}</Tag>
+                            }) : false
+                        }
+
+                        {
+                            this.state.yellowIs ? this.state.yellowData.map((item, key) => {
+                                return <Tag color="gold" style={{ margin: "4px 8px" }} key={key} onClick={this.handleTag.bind(this, item._id)}>{item.dormNum}</Tag>
+                            }) : false
+                        }
+
+                        {
+                            this.state.greenIs ? this.state.greenData.map((item, key) => {
+                                return <Tag color="green" style={{ margin: "4px 8px" }} key={key} onClick={this.handleTag.bind(this, item._id)}>{item.dormNum}</Tag>
+                            }) : false
+                        }
                     </Col>
                     <Col lg={{ span: 4 }}>
-                        <Badge count="5" style={{ background: "#fb1b32" }} className="dorm-fix-btn">
-                            <Button onClick={this.handleGetData}><Icon style={{ display: this.state.loadIcon }} type="loading" />获取</Button>
+                        <Badge count={this.state.isDormFixStateCount ? this.state.dormFixStateCount[0] : "..."} style={{ background: "#fb1b32" }} className="dorm-fix-btn">
+                            <Button disabled={this.state.redDisabled} onClick={this.handleGetData.bind(this, 0)}><Icon style={{ display: this.state.redLoadIcon }} type="loading" />获取</Button>
 
                         </Badge>
                         <br />
-                        <Badge count="5" style={{ background: "#0892f8" }} color="processing" className="dorm-fix-btn">
-                            <Button onClick={this.handleGetData}><Icon style={{ display: this.state.loadIcon }} type="loading" />获取</Button>
+
+                        <Badge count={this.state.isDormFixStateCount ? this.state.dormFixStateCount[1] : "..."} style={{ background: "#0892f8" }} color="processing" className="dorm-fix-btn">
+                            <Button disabled={this.state.blueDisabled} onClick={this.handleGetData.bind(this, 1)}><Icon style={{ display: this.state.blueLoadIcon }} type="loading" />获取</Button>
                         </Badge>
                         <br />
-                        <Badge count="5" style={{ background: "#fdac3d" }} color="warning" className="dorm-fix-btn">
-                            <Button onClick={this.handleGetData}><Icon style={{ display: this.state.loadIcon }} type="loading" />获取</Button>
+                        <Badge count={this.state.isDormFixStateCount ? this.state.dormFixStateCount[2] : "..."} style={{ background: "#fdac3d" }} color="warning" className="dorm-fix-btn">
+                            <Button disabled={this.state.yellowDisabled} onClick={this.handleGetData.bind(this, 2)}><Icon style={{ display: this.state.yellowLoadIcon }} type="loading" />获取</Button>
 
                         </Badge>
                         <br />
-                        <Badge count="5" style={{ background: "#52c41a" }} className="dorm-fix-btn">
-                            <Button disabled={this.state.getDataBtnState} onClick={this.handleGetData}><Icon style={{ display: this.state.loadIcon }} type="loading" />获取</Button>
+                        <Badge count={this.state.isDormFixStateCount ? this.state.dormFixStateCount[3] : "..."} style={{ background: "#52c41a" }} className="dorm-fix-btn">
+                            <Button disabled={this.state.greenDisabled} onClick={this.handleGetData.bind(this, 3)}><Icon style={{ display: this.state.greenLoadIcon }} type="loading" />获取</Button>
                         </Badge>
                     </Col>
                     <Col lg={{ span: 4 }}>
